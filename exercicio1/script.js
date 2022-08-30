5364c09916fed5eb0e1a63442af332e1a8764891
BarbaraAmaral27 <barbaramamaral@gmail.com>
2022-08-30 00:31:49 -0300

    primeiros exercicios html e js

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+let nome 
+let idade
+
+console.log(typeof idade, typeof nome)
+
+//variáveis sem valor = undefined
+
+const nomePrompt = prompt("Qual o seu nome?")
+
+let idadePrompt = prompt("Qual a sua idade?")
+
+console.log(typeof nomePrompt, typeof idadePrompt)
+
+console.log("O meu nome é:", nomePrompt, "Minha idade é:", idadePrompt)
+
+
